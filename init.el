0ad30c229bfff189920e254cf58eef72ab4f5c71
Daniel Reed <dan.reed@gsts.ca>
2020-11-02 10:28:44 -0400

    Mode line formatting added

--- init.el
+++ init.el
@@ -24,6 +24,11 @@
   (global-company-mode t)
 )
 
+;; Setup mode-line
+(require 'simple-modeline)
+(simple-modeline-mode 1)
+
+
 ; Parentheses
 (use-package highlight-parentheses
   :ensure t
@@ -38,7 +43,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(ace-window julia-repl julia-mode vterm highlight-parentheses company ess use-package)))
+   '(simple-modeline telephone-line ace-window julia-repl julia-mode vterm highlight-parentheses company ess use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
